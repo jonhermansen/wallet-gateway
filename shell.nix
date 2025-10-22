d93f025bfd5fb9769a063298fbcc971c200840f5
Jordan Cohen <31041063+jordan-da@users.noreply.github.com>
2025-10-22 13:27:21 -0400

    nixing helm stuff (#5)
    Co-authored-by: Jordan Cohen <hypergig@gmail.com>

---
+++ shell.nix
@@ -0,0 +1,23 @@
+let
+  rev = "a7f6fb78c74c145ed8210f0ac9d6a3fb595ba92f";
+  sha256 = "09xpkn3m6ckvcm5v1nvnrs37ayay3wiivnnpcrnfms90mq9m84na";
+
+  nixPkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+    inherit sha256;
+  }) { };
+
+in
+nixPkgs.mkShell (
+  with nixPkgs;
+  {
+    packages = [
+      (wrapHelm kubernetes-helm {
+        plugins = [
+          kubernetes-helmPlugins.helm-diff
+          kubernetes-helmPlugins.helm-unittest
+        ];
+      })
+    ];
+  }
+)
